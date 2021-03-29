bd1625218e272217e818bbf178b3a282ad82e2cc
evynprice <me@evynprice.com>
2021-03-29 15:12:32 -0400

    Update Help documentation

--- src/main/java/me/evyn/barista/core/commands/Help.java
+++ src/main/java/me/evyn/barista/core/commands/Help.java
@@ -32,11 +32,21 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Help implements Command {
+
+    /**
+     * Sends bot information in the event's channel
+     * @param event Message Event
+     * @param prefix Bot prefix
+     * @param embeds embedsEnabled
+     * @param args command arguments
+     */
     @Override
     public void run(MessageReceivedEvent event, String prefix, boolean embeds, String[] args) {
+        // fetch bot user
         User bot = event.getJDA().getSelfUser();
 
         if (embeds) {
+            // generate help embed
             EmbedBuilder eb = new EmbedBuilder()
                     .setTitle(bot.getName(), "https://barista.evyn.me")
                     .setColor(0xdbb381)
@@ -50,25 +60,38 @@ public class Help implements Command {
                     .addField("Commands", prefix + "commands", false)
                     .setFooter("Barista");
 
+            // send embed message
             event.getChannel()
                     .sendMessage(eb.build())
                     .queue();
         } else {
-            event.getChannel().sendMessage(
-                    "Barista is a simple yet effective, multi-purpose Discord bot written with JDA. It makes a " +
-                            "perfect addition to your community, study group, or hangout server, and it adds a bit of" +
-                            " fun and utility to any environment." + "\n\n" + "**GitHub:** <https://github.com/" +
-                            "evynprice/baristabot>" + "\n" + "**Version:** " + BaristaInfo.VERSION + "\n" + "**Support" +
-                            ":** <https://discord.gg/u8hAu6sEtw> " + "\n" + "**Commands:** " + prefix + "commands"
-            ).queue();
+            // generate plaintext message
+            String msg = "Barista is a simple yet effective, multi-purpose Discord bot written with JDA. It makes a " +
+                    "perfect addition to your community, study group, or hangout server, and it adds a bit of" +
+                    " fun and utility to any environment." + "\n\n" + "**GitHub:** <https://github.com/" +
+                    "evynprice/baristabot>" + "\n" + "**Version:** " + BaristaInfo.VERSION + "\n" + "**Support" +
+                    ":** <https://discord.gg/u8hAu6sEtw> " + "\n" + "**Commands:** " + prefix + "commands";
+
+            // send message
+            event.getChannel()
+                    .sendMessage(msg)
+                    .queue();
         }
     }
 
+    /**
+     * Provides name of command
+     * @return command name
+     */
     @Override
     public String getName() {
         return "help";
     }
 
+    /**
+     * Provides type of command
+     * @return command type
+     */
     @Override
     public CommandType getType() {
         return CommandType.CORE;
